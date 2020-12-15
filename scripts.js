85d2d3c38388ad28438aad57c8bcab4758b7a850
22school <72373185+22school@users.noreply.github.com>
2020-12-15 20:49:40 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#one').click(function () { // 1
+        elementClick = $(this).attr('href');
+        destination = $(elementClick).offset().top;
+        $('body,html').animate({scrollTop: destination }, 1000);
+    });
+});
+$(document).ready(function(){
+    $('#two').click(function () { // 2
+        elementClick = $(this).attr('href');
+        destination = $(elementClick).offset().top;
+        $('body,html').animate({scrollTop: destination }, 1000);
+    });
+});
+$(document).ready(function(){
+    $('#three').click(function () { // 3
+        elementClick = $(this).attr('href');
+        destination = $(elementClick).offset().top;
+        $('body,html').animate({scrollTop: destination }, 1000);
+    });
+});
+$(document).ready(function(){
+    $('#four').click(function () { // 4
+        elementClick = $(this).attr('href');
+        destination = $(elementClick).offset().top;
+        $('body,html').animate({scrollTop: destination }, 1000);
+    });
+});
